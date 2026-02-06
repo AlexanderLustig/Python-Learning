9a8f777a141bece82594bc70e5dac0582110063f
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2026-02-06 20:41:18 +0000

    commit

---
+++ --- Project.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+query = input("Please enter your IP address: ")
+r = requests.get(f'http://ip-api.com/json/{query}')
+
+with open("IP_JSON_PROJECT.json", "w") as f:
+    json.dump(r.json(), f, indent=4)
+
